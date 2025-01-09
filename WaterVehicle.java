47a7bc9310b006fa7ca6cbba4ba2d03fb9ae79fc
0pyrolytic <132107689+0pyrolytic@users.noreply.github.com>
2025-01-09 14:52:38 -0500

    Update WaterVehicle.java

--- WaterVehicle.java
+++ WaterVehicle.java
@@ -1,3 +1,37 @@
+//WaterVehicle Class with abstract methods methods and fields
 class WaterVehicle{
+private double fuelCapacity;
+private boolean isNuclearPowered;
+private String serialNum;
+private int speed;
+private String location;
+private int cost;
+private int partSwapWorth;
+private boolean isInWater;
 
+//Constructor
+  public WaterVehicle(double fuelCapacity, boolean isNuclearPowered, int manufactureYear, String serialNum, int speed, String location, int cost){
+    this.fuelCapacity = fuelCapacity;
+    this.isNuclearPowered = isNuclearPowered;
+    this.manufactureYear = manufactureYear;
+    this.serialNum = serialNum;
+    this.speed = speed;
+    this.location = location;
+    this.cost = cost;
+    isInwater = false;
+  }
+
+  public compareWater(WaterVehicle){
+    
+  }
+
+  public generateSerialNum(){
+  }
+
+  public String toString(){
+  }
+
+  public boolean launch(){
+    
+  }
 }
