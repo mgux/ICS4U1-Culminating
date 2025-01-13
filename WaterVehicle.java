aebc3fbe41e28a111428f6d19abe0851a147e5ee
0pyrolytic <132107689+0pyrolytic@users.noreply.github.com>
2025-01-13 11:10:20 -0500

    Update WaterVehicle.java

--- WaterVehicle.java
+++ WaterVehicle.java
@@ -1,37 +1,98 @@
-//WaterVehicle Class with abstract methods methods and fields
-class WaterVehicle{
-private double fuelCapacity;
-private boolean isNuclearPowered;
-private String serialNum;
-private int speed;
-private String location;
-private int cost;
-private int partSwapWorth;
-private boolean isInWater;
+// WaterVehicle Class with abstract methods and fields
+class WaterVehicle {
+    private double fuelCapacity;
+    private boolean isNuclearPowered;
+    private int manufactureYear;
+    private String serialNum;
+    private int speed;
+    private String location;
+    private int cost;
+    private int partSwapWorth;
+    private boolean isInWater;
 
-//Constructor
-  public WaterVehicle(double fuelCapacity, boolean isNuclearPowered, int manufactureYear, String serialNum, int speed, String location, int cost){
-    this.fuelCapacity = fuelCapacity;
-    this.isNuclearPowered = isNuclearPowered;
-    this.manufactureYear = manufactureYear;
-    this.serialNum = serialNum;
-    this.speed = speed;
-    this.location = location;
-    this.cost = cost;
-    isInwater = false;
-  }
+    // Constructor
+    public WaterVehicle(double fuelCapacity, boolean isNuclearPowered, int manufactureYear, String serialNum, int speed, String location, int cost) {
+        this.fuelCapacity = fuelCapacity;
+        this.isNuclearPowered = isNuclearPowered;
+        this.manufactureYear = manufactureYear;
+        this.serialNum = serialNum;
+        this.speed = speed;
+        this.location = location;
+        this.cost = cost;
+        this.isInWater = false;
+    }
 
-  public compareWater(WaterVehicle){
-    
-  }
+    // Accessor (Getter) Methods
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
 
-  public generateSerialNum(){
-  }
+    public boolean getIsNuclearPowered() {
+        return isNuclearPowered;
+    }
 
-  public String toString(){
-  }
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
 
-  public boolean launch(){
-    
-  }
+    public String getSerialNum() {
+        return serialNum;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getPartSwapWorth() {
+        return partSwapWorth;
+    }
+
+    public boolean getIsInWater() {
+        return isInWater;
+    }
+
+    // Mutator (Setter) Methods
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public void setIsNuclearPowered(boolean isNuclearPowered) {
+        this.isNuclearPowered = isNuclearPowered;
+    }
+
+    public void setManufactureYear(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public void setSerialNum(String serialNum) {
+        this.serialNum = serialNum;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setPartSwapWorth(int partSwapWorth) {
+        this.partSwapWorth = partSwapWorth;
+    }
+
+    public void setIsInWater(boolean isInWater) {
+        this.isInWater = isInWater;
+    }
 }
