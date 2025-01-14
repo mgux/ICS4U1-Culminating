9591ca1b37a51578eae2ba76c430a316e0a0b942
0pyrolytic <132107689+0pyrolytic@users.noreply.github.com>
2025-01-14 09:43:13 -0500

    Update WaterVehicle.java

--- WaterVehicle.java
+++ WaterVehicle.java
@@ -1,5 +1,5 @@
 // WaterVehicle Class with abstract methods and fields
-class WaterVehicle {
+public abstract class WaterVehicle {
     private double fuelCapacity;
     private boolean isNuclearPowered;
     private int manufactureYear;
@@ -95,4 +95,74 @@ class WaterVehicle {
     public void setIsInWater(boolean isInWater) {
         this.isInWater = isInWater;
     }
+
+    //class methods
+    public boolean launch(){
+        if(isInWater){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //abstract methods
+    public abstract String generateSerialNum();
+
+}
+
+import java.util.ArrayList;
+
+public class Ship extends WaterVehicle {
+    // Class fields
+    private int buoyancy;
+    private int numberOfGuns;
+    private String type;
+    private int maxTankStorage;
+    private int maxJetStorage;
+    private int maxSubmarineStorage;
+    private int maxRocketStorage;
+    private boolean docked;
+    private ArrayList<Tank> tankInShip; 
+    private ArrayList<Jet> jetInShip; 
+    private ArrayList<Submarine> submarineInShip; 
+    private ArrayList<Rocket> rocketInShip; 
+
+    // Constructor
+    public Ship(double fuelCapacity, boolean isNuclearPowered, int manufactureYear, String serialNum, int speed, String location, int cost,
+                int buoyancy, int numberOfGuns, String type, int maxTankStorage, int maxJetStorage, int maxSubmarineStorage, int maxRocketStorage) {
+        super(fuelCapacity, isNuclearPowered, manufactureYear, serialNum, speed, location, cost);
+        this.buoyancy = buoyancy;
+        this.numberOfGuns = numberOfGuns;
+        this.type = type;
+        this.maxTankStorage = maxTankStorage;
+        this.tankInShip = new ArrayList<>(); 
+        this.jetInShip = new ArrayList<>(); 
+        this.submarineInShip = new ArrayList<>(); 
+        this.rocketInShip = new ArrayList<>(); 
+        this.docked = true; 
+    }
+
+    public boolean addTank(Tank tank) {
+        String serialNumOfTank = tank.getSerialNum();
+
+        if (!LandManager.isCarried(serialNumOfTank)) {
+            if (tankInShip.size() < maxTankStorage) {
+                tankInShip.add(tank);
+                return true;
+            } else {
+                System.out.println("Cannot add Tank. Maximum tank storage reached!");
+                return false;
+            }
+        } else {
+            System.out.println("Tank with serial number " + serialNumOfTank + " is already on the ship.");
+            return false;
+        }
+    }
+}
+
+
+public class Submarine extends WaterVehicle{
+    private int depth;
+    private int numberIfTorpedos;
+    private int underWaterVisibility;
 }
