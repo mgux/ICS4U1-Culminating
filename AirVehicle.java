eca276cb722d9856f991c2961ad8b2ab4be48b91
mgux <105395318+mgux@users.noreply.github.com>
2025-01-15 10:47:03 -0500

    Update AirVehicle.java

--- AirVehicle.java
+++ AirVehicle.java
@@ -9,7 +9,13 @@ public abstract class AirVehicle {
     private int altitude;
     private int partSwapWorth;
 
-    public AirVehicle(int engineNum, int wingNum, int manufactureYear, String serialNum, int speed, String location, int cost, int altitude, int partSwapWorth) {
+
+
+    private int parts;
+    private int maxParts;
+    private int minParts;
+
+    public AirVehicle(int engineNum, int wingNum, int manufactureYear, String serialNum, int speed, String location, int cost, int altitude, int partSwapWorth, int parts, int maxParts,int minParts) {
         this.engineNum = engineNum;
         this.wingNum = wingNum;
         this.manufactureYear = manufactureYear;
@@ -19,6 +25,9 @@ public abstract class AirVehicle {
         this.cost = cost;
         this.altitude = altitude;
         this.partSwapWorth = partSwapWorth;
+        this.parts = parts;
+        this.minParts = minParts;
+        this.maxParts = maxParts;
     }
 
     public boolean launch() {
@@ -98,6 +107,22 @@ public abstract class AirVehicle {
 
     public void setEngineNum(int engineNum) {
         this.engineNum = engineNum;
+    }
+
+    public int getParts() {
+        return parts;
+    }
+
+    public int getMaxParts() {
+        return maxParts;
+    }
+
+    public int getMinParts() {
+        return minParts;
+    }
+
+    public int compareAltitude(AirVehicle air) {
+        return this.altitude - air.altitude;
     }
 
     public String toString() {
