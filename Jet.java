1d30ddfb7868ff2fbe2b2c3b39fe355dfef01fa5
mgux <105395318+mgux@users.noreply.github.com>
2025-01-15 11:31:21 -0500

    Add files via upload

---
+++ Jet.java
@@ -0,0 +1,24 @@
+public class Jet extends AirVehicle{
+    private String missileType;
+    private int agility;
+    private int storageTaken;
+
+    public Jet(int engineNum, int wingNum, int manufactureYear, String serialNum, int speed, String location, int cost, int altitude, int partSwapWorth, String missileType, int agility, int storageTaken, int parts, int minParts, int maxParts) {
+        super(engineNum, wingNum, manufactureYear, serialNum, speed, location, cost, altitude, partSwapWorth,parts,minParts,maxParts);
+        this.agility = agility;
+        this.missileType = missileType;
+        this.storageTaken = storageTaken;
+    }
+
+    public int compareAgility(Jet jet) {
+        return this.agility - jet.agility;
+    }
+
+    public String toString() {
+        return "Jet{" +
+                "missileType='" + missileType + '\'' +
+                ", agility=" + agility +
+                ", storageTaken=" + storageTaken +
+                '}';
+    }
+}
