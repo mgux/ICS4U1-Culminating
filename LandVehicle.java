c41fd994b88232da86995e98ee8d412985b1e6ac
Mesuckatk <tianyue.zhao@student.tdsb.on.ca>
2025-01-16 21:08:32 -0500

    Update LandVehicle.java

--- LandVehicle.java
+++ LandVehicle.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public abstract class LandVehicle
 {
    private int tires;
@@ -11,6 +13,7 @@ public abstract class LandVehicle
    private int maxParts;
    private int minParts;
    final static int PART_SWAP_WORTH = 4;
+   private ArrayList<Repairs> repairHistory = new ArrayList<Repairs>();
   
    public LandVehicle(int n1, int n2, int n3, String s1, int n4, String s2, int n5, int n6, int n7, int n8)
    {
@@ -78,7 +81,7 @@ public abstract class LandVehicle
    {
       manufactureYear = n;
    }
-   public void getSerialNum(String s)
+   public void setSerialNum(String s)
    {
       serialNum = s;
    }
@@ -86,7 +89,7 @@ public abstract class LandVehicle
    {
       speed = n;
    }
-   public void getLocation(String s)
+   public void setLocation(String s)
    {
       location = s;
    }
@@ -123,6 +126,23 @@ public abstract class LandVehicle
    }
    
    public abstract boolean isBroken();
+   
+   public boolean addRepair()
+   {
+      Repairs r = new Repairs(this);
+      repairHistory.add(r);
+      return true;
+   }
+   
+   public Repairs getMostRecentRepair()
+   {
+      Repairs r = repairHistory.get(0);
+      for (int i = 1; i < repairHistory.size(); i++)
+      {
+         r = r.compareRepairTime(repairHistory.get(i));
+      }
+      return r;
+   }
    
    public boolean removeParts(int n)
    {
