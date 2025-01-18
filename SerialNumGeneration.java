558f495535c260bcbbdb661b2355c53927c6e36d
Mesuckatk <tianyue.zhao@student.tdsb.on.ca>
2025-01-17 22:04:42 -0500

    Add files via upload

---
+++ SerialNumGeneration.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class SerialNumGeneration
+{
+   final static ArrayList<String> LAND_VEHICLE_SERIALS = new ArrayList<String>();
+   final static ArrayList<String> WATER_VEHICLE_SERIALS = new ArrayList<String>();
+   final static ArrayList<String> AIR_VEHICLE_SERIALS = new ArrayList<String>();
+   final static ArrayList<String> SPACE_VEHICLE_SERIALS = new ArrayList<String>();
+   final static int DIGITS = 10;
+   final static int LAND_VEHICLE_SERIAL_SIZE = 6;
+   final static int WATER_VEHICLE_SERIAL_SIZE = 0;
+   final static int AIR_VEHICLE_SERIAL_SIZE = 0;
+   final static int SPACE_VEHICLE_SERIAL_SIZE = 0;
+   
+   public static void landVehicleGeneration()
+   {
+      String s = generateLandVehicle("");
+      while (!addLandVehicleSerial(s))
+      {
+         s = generateLandVehicle("");
+      }
+   }
+   public static String waterVehicleGeneration()
+   {
+   
+   }
+   public static String airVehicleGeneration()
+   {
+   
+   }
+   public static String spaceVehicleGeneration()
+   {
+   
+   }
+   
+   public static String generateLandVehicle(String s)
+   {
+      if (s.length() == LAND_VEHICLE_SERIAL_SIZE)
+      {
+         return s;
+      }
+      return generateLandVehicle(s+(int)(Math.random()*DIGITS));
+   }
+   
+   public static boolean addLandVehicleSerial(String s)
+   {
+      boolean isFound = false;
+      int mid, l = 0, r = LAND_VEHICLE_SERIALS.size();
+      while (l <= r && !isFound)
+      {
+         mid = (l+r)/2;
+         if (LAND_VEHICLE_SERIALS.get(mid).equals(s))
+         {
+            isFound = true;
+         }
+         if (LAND_VEHICLE_SERIALS.get(mid).compareTo(s) > 0)
+         {
+            r = mid-1;
+         }
+         else
+         {
+            l = mid+1;
+         }
+      }
+      if (!isFound)
+      {
+         LAND_VEHICLE_SERIALS.add(l, s);
+      }
+      return (!isFound);
+   }
+}
