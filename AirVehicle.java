498cae7fef8290a6acef64572c7d6a853dd36268
mgux <105395318+mgux@users.noreply.github.com>
2025-01-20 12:27:34 -0500

    Update AirVehicle.java

--- AirVehicle.java
+++ AirVehicle.java
@@ -1,174 +1,187 @@
+import java.util.ArrayList;
+
 public abstract class AirVehicle {
-   private int wingNum;
-   private int manufactureYear;
-   private String serialNum;
-   private int speed;
+    final static int PART_SWAP_WORTH = 3;
+    private int wingNum;
+    private int manufactureYear;
+    private String serialNum;
+    private int speed;
 
-   private String location;
-   private int cost;
-   private int altitude;
-   private int partSwapWorth;
+    private String location;
+    private int cost;
+    private int altitude;
 
 
+    private int parts;
+    private int maxParts;
+    private int minParts;
 
-   private int parts;
-   private int maxParts;
-   private int minParts;
+    private ArrayList<Repairs> repairHistory = new ArrayList<>();
 
-   public AirVehicle(int engineNum, int wingNum, int manufactureYear, int speed, String location, int cost, int altitude, int partSwapWorth, int parts, int maxParts,int minParts) {
-      this.engineNum = engineNum;
-      this.wingNum = wingNum;
-      this.manufactureYear = manufactureYear;
-      this.speed = speed;
-      this.location = location;
-      this.cost = cost;
-      this.altitude = altitude;
-      this.partSwapWorth = partSwapWorth;
-      this.parts = parts;
-      this.minParts = minParts;
-      this.maxParts = maxParts;
-   }
+    public AirVehicle(int engineNum, int wingNum, int manufactureYear, int speed, String location, int cost, int altitude, int parts, int maxParts,int minParts) {
+        this.engineNum = engineNum;
+        this.wingNum = wingNum;
+        this.manufactureYear = manufactureYear;
+        this.speed = speed;
+        this.location = location;
+        this.cost = cost;
+        this.altitude = altitude;
+        this.parts = parts;
+        this.minParts = minParts;
+        this.maxParts = maxParts;
+    }
 
-   public boolean launch() {
-      return true;
-   }
+    private int engineNum;
 
-   private int engineNum;
+    public int getEngineNum() {
+        return engineNum;
+    }
 
-   public int getEngineNum() {
-      return engineNum;
-   }
+    public int getWingNum() {
+        return wingNum;
+    }
 
-   public int getWingNum() {
-      return wingNum;
-   }
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
 
-   public int getManufactureYear() {
-      return manufactureYear;
-   }
+    public String getSerialNum() {
+        return serialNum;
+    }
 
-   public String getSerialNum() {
-      return serialNum;
-   }
+    public int getSpeed() {
+        return speed;
+    }
 
-   public int getSpeed() {
-      return speed;
-   }
+    public String getLocation() {
+        return location;
+    }
 
-   public String getLocation() {
-      return location;
-   }
+    public int getCost() {
+        return cost;
+    }
 
-   public int getCost() {
-      return cost;
-   }
+    public int getAltitude() {
+        return altitude;
+    }
 
-   public int getAltitude() {
-      return altitude;
-   }
+    public int getPartSwapWorth() {
+        return PART_SWAP_WORTH;
+    }
 
-   public int getPartSwapWorth() {
-      return partSwapWorth;
-   }
 
-   public void setWingNum(int wingNum) {
-      this.wingNum = wingNum;
-   }
+    public void setWingNum(int wingNum) {
+        this.wingNum = wingNum;
+    }
 
-   public void setManufactureYear(int manufactureYear) {
-      this.manufactureYear = manufactureYear;
-   }
+    public void setManufactureYear(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
 
-   public void setSerialNum(String serialNum) {
-      this.serialNum = serialNum;
-   }
+    public void setSerialNum(String serialNum) {
+        this.serialNum = serialNum;
+    }
 
-   public void setSpeed(int speed) {
-      this.speed = speed;
-   }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
-   public void setLocation(String location) {
-      this.location = location;
-   }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-   public void setCost(int cost) {
-      this.cost = cost;
-   }
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 
-   public void setAltitude(int altitude) {
-      this.altitude = altitude;
-   }
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
 
-   public void setPartSwapWorth(int partSwapWorth) {
-      this.partSwapWorth = partSwapWorth;
-   }
+    public void setEngineNum(int engineNum) {
+        this.engineNum = engineNum;
+    }
 
-   public void setEngineNum(int engineNum) {
-      this.engineNum = engineNum;
-   }
+    public void setParts(int parts) {
+        this.parts = parts;
+    }
 
-   public int getParts() {
-      return parts;
-   }
+    public void setMaxParts(int maxParts) {
+        this.maxParts = maxParts;
+    }
 
-   public int getMaxParts() {
-      return maxParts;
-   }
+    public void setMinParts(int minParts) {
+        this.minParts = minParts;
+    }
 
-   public int getMinParts() {
-      return minParts;
-   }
-   
-   public void setParts(int n)
-   {
-      parts = n;
-   }
-   
-   public void setMaxParts(int n)
-   {
-      maxParts = n;
-   }
-   
-   public void setMinParts(int n)
-   {
-      minParts = n;
-   }
+    public int getParts() {
+        return parts;
+    }
 
-   public int compareAltitude(AirVehicle air) {
-      return this.altitude - air.altitude;
-   }
+    public int getMaxParts() {
+        return maxParts;
+    }
 
-   public int compareEngineNum(AirVehicle air) {
-      return this.engineNum - air.engineNum;
-   }
+    public int getMinParts() {
+        return minParts;
+    }
 
-   public int compareWingNum(AirVehicle air) {
-      return this.wingNum - air.wingNum;
-   }
+    public int compareAltitude(AirVehicle air) {
+        return this.altitude - air.altitude;
+    }
 
-   public int compareSpeed(AirVehicle air) {
-      return this.speed - air.speed;
-   }
+    public int compareEngineNum(AirVehicle air) {
+        return this.engineNum - air.engineNum;
+    }
 
-   public boolean isBroken() {
-      if (parts < minParts) {
-         return true;
-      }
-   
-      return false;
-   }
+    public int compareWingNum(AirVehicle air) {
+        return this.wingNum - air.wingNum;
+    }
 
-   public String toString() {
-      return "AirVehicle{" +
-             "wingNum=" + wingNum +
-             ", manufactureYear=" + manufactureYear +
-             ", serialNum='" + serialNum + '\'' +
-             ", speed=" + speed +
-             ", location='" + location + '\'' +
-             ", cost=" + cost +
-             ", altitude=" + altitude +
-             ", partSwapWorth=" + partSwapWorth +
-             ", engineNum=" + engineNum +
-             '}';
-   }
+    public int compareSpeed(AirVehicle air) {
+        return this.speed - air.speed;
+    }
+
+    public int compareCost(AirVehicle air) {
+        return this.cost - air.cost;
+    }
+
+    public boolean addRepair()
+    {
+        Repairs r = new Repairs(this);
+        repairHistory.add(r);
+        return true;
+    }
+
+    public boolean removeParts(int n)
+    {
+        if (parts-n < minParts)
+        {
+            return false;
+        }
+        setParts(parts-n);
+        return true;
+    }
+
+    public boolean isBroken() {
+        if (parts < minParts) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String toString() {
+        return "AirVehicle{" +
+                "wingNum=" + wingNum +
+                ", manufactureYear=" + manufactureYear +
+                ", serialNum='" + serialNum + '\'' +
+                ", speed=" + speed +
+                ", location='" + location + '\'' +
+                ", cost=" + cost +
+                ", altitude=" + altitude +
+                ", partSwapWorth=" + PART_SWAP_WORTH +
+                ", engineNum=" + engineNum +
+                '}';
+    }
 }
