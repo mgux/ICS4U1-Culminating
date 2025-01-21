9f603baf6cf9416c264d558787028a55dcef2866
Mesuckatk <tianyue.zhao@student.tdsb.on.ca>
2025-01-21 09:14:27 -0500

    Update LandVehicle.java

--- LandVehicle.java
+++ LandVehicle.java
@@ -15,7 +15,7 @@ public abstract class LandVehicle
     final static int PART_SWAP_WORTH = 4;
     private ArrayList<Repairs> repairHistory = new ArrayList<Repairs>();
 
-    public LandVehicle(int n1, int n2, int n3, String s1, int n4, String s2, int n5, int n6, int n7, int n8)
+    public LandVehicle(int n1, int n2, int n3, String s1, int n4, String s2, int n5, int n6, int n7, int n8) //Constructor
     {
         tires = n1;
         seats = n2;
@@ -29,6 +29,7 @@ public abstract class LandVehicle
         minParts = n8;
     }
 
+    //Accessors and Mutators
     public int getTires()
     {
         return tires;
@@ -110,31 +111,31 @@ public abstract class LandVehicle
         minParts = n;
     }
 
-    public LandVehicle compareSpeed(LandVehicle other)
+    public LandVehicle compareSpeed(LandVehicle other) //Returns LandVehicle with the higher speed between the two; In case of tie, return this
     {
         return ((this.getSpeed()>other.getSpeed())?this:other);
     }
 
-    public LandVehicle compareCost(LandVehicle other)
+    public LandVehicle compareCost(LandVehicle other) //Returns the LandVehicle with the higher cost; In case of tie, return this
     {
         return ((this.getCost()>other.getCost())?this:other);
     }
 
-    public LandVehicle compareSeats(LandVehicle other)
+    public LandVehicle compareSeats(LandVehicle other) //Returns the LandVehicle with the higher number of seats; In case of tie, return this
     {
         return ((this.getSeats()>other.getSeats())?this:other);
     }
 
-    public abstract boolean isBroken();
+    public abstract boolean isBroken();  //Abstract method to check whether the vehicle is broken
 
-    public boolean addRepair()
+    public boolean addRepair() //Repair the Vehicle and add it to repairHistory
     {
         Repairs r = new Repairs(this);
         repairHistory.add(r);
         return true;
     }
 
-    public Repairs getMostRecentRepair()
+    public Repairs getMostRecentRepair() //Returns the most recent repair by Date
     {
         Repairs r = repairHistory.get(0);
         for (int i = 1; i < repairHistory.size(); i++)
@@ -144,7 +145,7 @@ public abstract class LandVehicle
         return r;
     }
 
-    public boolean removeParts(int n)
+    public boolean removeParts(int n) //Removes parts from the vehicle
     {
         if (parts-n < minParts)
         {
@@ -154,7 +155,7 @@ public abstract class LandVehicle
         return true;
     }
 
-    public String toString()
+    public String toString() //ToString Method
     {
         return tires+"\n"+seats+"\n"+manufactureYear+"\n"+serialNum+"\n"+speed+"\n"+location+"\n"+cost+"\n" + parts + "\n" + maxParts + "\n" + minParts + "\n";
     }
